62a8b164fbdf0255703b7dcb77538b0d3dec6d79
sunsetsavorer <sunset.savorer@gmail.com>
2025-06-05 14:49:02 +0300

    chore: global exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,29 +2,50 @@
 
 namespace App\Exceptions;
 
+use App\Domains\Common\Exceptions\ServiceException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
-    protected $dontFlash = [
-        'current_password',
-        'password',
-        'password_confirmation',
-    ];
+	/**
+	 * The list of the inputs that are never flashed to the session on validation exceptions.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $dontFlash = [
+		'current_password',
+		'password',
+		'password_confirmation',
+	];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     */
-    public function register(): void
-    {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
-    }
+	public function register(): void
+	{
+		$this->reportable(function (Throwable $e) {
+			//
+		});
+	}
+
+	public function render($request, Throwable $exception)
+	{
+		list($message, $code) = match (get_class($exception)) {
+			ServiceException::class => [
+				['other' => $exception->getMessage()],
+				$exception->getCode(),
+			],
+			ValidationException::class => [
+				$exception->validator->errors()->getMessages(),
+				422,
+			],
+			default => [
+				$exception->getMessage(),
+				400,
+			],
+		};
+
+		return response()->json([
+			'errors' => $message,
+		], $code);
+	}
 }
