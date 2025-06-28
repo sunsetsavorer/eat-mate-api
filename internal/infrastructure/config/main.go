4f20f29859839c13621950a90856aaa083d0d76a
sunsetsavorer <sunset.savorer@gmail.com>
2025-06-28 22:40:41 +0300

    feat: add `config`

---
+++ internal/infrastructure/config/main.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	AppPort      string
+	Debug        bool
+	AllowOrigins []string
+	DbConn       string
+}
+
+func NewConfig() *Config {
+
+	return &Config{}
+}
+
+func (cfg *Config) LoadConfig() error {
+
+	viper.SetConfigFile("config/.env")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return fmt.Errorf("failed to read config: %v", err)
+	}
+
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return fmt.Errorf("failed to parse config: %v", err)
+	}
+
+	return nil
+}
