a03b492d455455dc50092df56a48e61ed2f3e17a
sunsetsavorer <sunset.savorer@gmail.com>
2025-06-28 22:55:04 +0300

    feat: add logger to handler

--- internal/handlers/http/base_handler.go
+++ internal/handlers/http/base_handler.go
@@ -3,20 +3,24 @@ package http
 import (
 	"github.com/sunsetsavorer/eat-mate-api/internal/infrastructure/config"
 	"github.com/sunsetsavorer/eat-mate-api/internal/infrastructure/db"
+	"github.com/sunsetsavorer/eat-mate-api/internal/infrastructure/logger"
 )
 
 type BaseHdlr struct {
 	db     *db.Db
 	config *config.Config
+	logger *logger.Logger
 }
 
 func NewBaseHdlr(
 	db *db.Db,
 	config *config.Config,
+	logger *logger.Logger,
 ) *BaseHdlr {
 
 	return &BaseHdlr{
 		db:     db,
 		config: config,
+		logger: logger,
 	}
 }
