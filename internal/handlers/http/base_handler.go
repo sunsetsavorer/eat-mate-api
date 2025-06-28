1e075446fe9bdd35060954e34035a0d7f49aafb4
sunsetsavorer <sunset.savorer@gmail.com>
2025-06-28 22:29:16 +0300

    feat: add base handler

--- internal/handlers/http/base_handler.go
+++ internal/handlers/http/base_handler.go
@@ -1,9 +1,22 @@
 package http
 
+import (
+	"github.com/sunsetsavorer/eat-mate-api/internal/infrastructure/config"
+	"github.com/sunsetsavorer/eat-mate-api/internal/infrastructure/db"
+)
+
 type BaseHdlr struct {
+	db     *db.Db
+	config *config.Config
 }
 
-func NewBaseHdlr() *BaseHdlr {
+func NewBaseHdlr(
+	db *db.Db,
+	config *config.Config,
+) *BaseHdlr {
 
-	return &BaseHdlr{}
+	return &BaseHdlr{
+		db:     db,
+		config: config,
+	}
 }
