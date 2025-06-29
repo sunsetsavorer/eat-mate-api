550436fb1d366d0f7a355879603be2e16cdaa5e5
sunsetsavorer <sunset.savorer@gmail.com>
2025-06-29 15:44:47 +0300

    feat: add router

---
+++ internal/app/router.go
@@ -0,0 +1,43 @@
+package app
+
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/sunsetsavorer/eat-mate-api/internal/handlers/http"
+)
+
+func (app App) InitRouter() *gin.Engine {
+
+	if !app.config.Debug {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	router := gin.New()
+	router.Use(
+		cors.New(
+			cors.Config{
+				AllowOrigins:     app.config.AllowOrigins,
+				AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "PATCH"},
+				AllowHeaders:     []string{"Content-Type", "Content-Length", "Accept-Encoding", "Authorization", "Cache-Control"},
+				ExposeHeaders:    []string{"Content-Length"},
+				AllowCredentials: true,
+				MaxAge:           12 * time.Hour,
+			},
+		),
+	)
+
+	baseHdlr := http.NewBaseHdlr(
+		app.db,
+		app.config,
+		app.logger,
+	)
+
+	v1 := router.Group("v1")
+	{
+
+	}
+
+	return router
+}
