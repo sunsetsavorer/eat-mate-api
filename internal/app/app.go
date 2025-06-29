5060d6be1addf0762a269a4c0f1d70178f28d207
sunsetsavorer <sunset.savorer@gmail.com>
2025-06-29 15:45:20 +0300

    feat: add app

---
+++ internal/app/app.go
@@ -0,0 +1,59 @@
+package app
+
+import (
+	"context"
+
+	"github.com/sunsetsavorer/eat-mate-api/internal/infrastructure/config"
+	"github.com/sunsetsavorer/eat-mate-api/internal/infrastructure/db"
+	"github.com/sunsetsavorer/eat-mate-api/internal/infrastructure/logger"
+	"github.com/sunsetsavorer/eat-mate-api/internal/infrastructure/server"
+)
+
+type App struct {
+	db     *db.Db
+	config *config.Config
+	logger *logger.Logger
+}
+
+func NewApp() *App {
+
+	return &App{}
+}
+
+func (app *App) InitInfrastructure() {
+
+	app.logger = logger.NewLogger()
+
+	config := config.NewConfig()
+	err := config.LoadConfig()
+
+	if err != nil {
+		app.logger.Errorf("an error occured while load config: %v", err)
+	}
+
+	app.config = config
+
+	db, err := db.NewDB(app.config.DbConn)
+
+	if err != nil {
+		app.logger.Errorf("db connection error: %v", err)
+	}
+
+	app.db = db
+}
+
+func (app App) ExecAndLoop() {
+
+	router := app.InitRouter()
+
+	srv := server.NewServer(app.config.AppPort, router)
+
+	err := srv.Start()
+
+	if err != nil {
+		app.logger.Errorf("server start error: %v", err)
+		return
+	}
+
+	defer srv.Stop(context.TODO())
+}
