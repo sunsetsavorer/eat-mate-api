542c29e742d0bd82c690aa7fea208fd1b411d944
sunsetsavorer <sunset.savorer@gmail.com>
2025-07-19 21:11:52 +0300

    feat: add `get user id` in base handler

--- internal/handlers/http/base_handler.go
+++ internal/handlers/http/base_handler.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/sunsetsavorer/eat-mate-api/internal/infrastructure/config"
 	"github.com/sunsetsavorer/eat-mate-api/internal/infrastructure/db"
 	"github.com/sunsetsavorer/eat-mate-api/internal/infrastructure/logger"
@@ -27,4 +28,13 @@ func NewBaseHdlr(
 		logger:    logger,
 		validator: validator,
 	}
+}
+
+func (hdlr BaseHdlr) GetUserID(c *gin.Context) (int64, bool) {
+
+	if userID, ok := c.Get("user_id"); ok {
+		return userID.(int64), true
+	}
+
+	return 0, false
 }
