978225bb664857990c2c2d1107d5c9af54fa9bdd
sunsetsavorer <sunset.savorer@gmail.com>
2025-07-19 19:20:04 +0300

    refactor: base contoller (get error)

--- internal/handlers/http/base_handler.go
+++ internal/handlers/http/base_handler.go
@@ -1,10 +1,6 @@
 package http
 
 import (
-	"fmt"
-	"net/http"
-
-	"github.com/sunsetsavorer/eat-mate-api/internal/exceptions"
 	"github.com/sunsetsavorer/eat-mate-api/internal/infrastructure/config"
 	"github.com/sunsetsavorer/eat-mate-api/internal/infrastructure/db"
 	"github.com/sunsetsavorer/eat-mate-api/internal/infrastructure/logger"
@@ -27,27 +23,4 @@ func NewBaseHdlr(
 		config: config,
 		logger: logger,
 	}
-}
-
-func (h BaseHdlr) getError(err error) (int, any) {
-
-	if _, ok := err.(*exceptions.ManyRequestsError); ok {
-		return http.StatusTooManyRequests, ErrorResp[OtherError]{
-			Errors: OtherError{err.Error()},
-		}
-	}
-
-	if _, ok := err.(*exceptions.NotFoundError); ok {
-		return http.StatusNotFound, ErrorResp[OtherError]{
-			Errors: OtherError{err.Error()},
-		}
-	}
-
-	if _, ok := err.(*exceptions.BadRequestError); ok {
-		return http.StatusBadRequest, ErrorResp[OtherError]{
-			Errors: OtherError{err.Error()},
-		}
-	}
-
-	return http.StatusBadRequest, fmt.Errorf("unknow response")
 }
