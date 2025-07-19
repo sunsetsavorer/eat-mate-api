689a241b9598ac674a26728cca9d55b9e971d524
sunsetsavorer <sunset.savorer@gmail.com>
2025-07-19 19:16:07 +0300

    refactor: move `getError` from base handler to infra

---
+++ internal/infrastructure/httpresp/contracts.go
@@ -0,0 +1,26 @@
+package httpresp
+
+type (
+	OtherError struct {
+		Other string `json:"other"`
+	}
+
+	ErrorResp[T OtherError | map[string]string] struct {
+		Errors T `json:"errors"`
+	}
+
+	SuccessResp struct {
+		Success bool `json:"success"`
+	}
+
+	SuccessDataResp struct {
+		Data any `json:"data"`
+	}
+)
+
+const (
+	TypeOtherError      = "other"
+	TypeValidationError = "validation"
+	TypeSuccessResp     = "success"
+	TypeDataResp        = "data"
+)
---
+++ internal/infrastructure/httpresp/main.go
@@ -0,0 +1,31 @@
+package httpresp
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/sunsetsavorer/eat-mate-api/internal/exceptions"
+)
+
+func GetError(err error) (int, any) {
+
+	if _, ok := err.(*exceptions.ManyRequestsError); ok {
+		return http.StatusTooManyRequests, ErrorResp[OtherError]{
+			Errors: OtherError{err.Error()},
+		}
+	}
+
+	if _, ok := err.(*exceptions.NotFoundError); ok {
+		return http.StatusNotFound, ErrorResp[OtherError]{
+			Errors: OtherError{err.Error()},
+		}
+	}
+
+	if _, ok := err.(*exceptions.BadRequestError); ok {
+		return http.StatusBadRequest, ErrorResp[OtherError]{
+			Errors: OtherError{err.Error()},
+		}
+	}
+
+	return http.StatusBadRequest, fmt.Errorf("unknow response")
+}
