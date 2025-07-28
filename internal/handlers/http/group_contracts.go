15e4f8435547fd3d02daf1ccf804051455f70040
sunsetsavorer <sunset.savorer@gmail.com>
2025-07-28 22:07:00 +0300

    feat: add `group` contracts

---
+++ internal/handlers/http/group_contracts.go
@@ -0,0 +1,13 @@
+package http
+
+import "github.com/google/uuid"
+
+type (
+	CreateGroupRequest struct {
+		Name               string      `json:"name" validate:"required"`
+		SelectionMode      string      `json:"selection_mode" validate:"required"`
+		IsPublic           bool        `json:"is_public" validate:"required"`
+		PlaceBranchID      uuid.UUID   `json:"place_branch_id"`
+		PlaceBranchOptions []uuid.UUID `json:"place_branch_options"`
+	}
+)
