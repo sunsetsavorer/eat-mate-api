80ce8a3f310fd8011de7f736bec77a772a1d0c21
sunsetsavorer <sunset.savorer@gmail.com>
2025-07-29 23:43:36 +0300

    feat: add `group` repo

---
+++ internal/repositories/group_repository.go
@@ -0,0 +1,63 @@
+package repositories
+
+import (
+	"github.com/sunsetsavorer/eat-mate-api/internal/entities"
+	"github.com/sunsetsavorer/eat-mate-api/internal/exceptions"
+	"github.com/sunsetsavorer/eat-mate-api/internal/infrastructure/db"
+	"github.com/sunsetsavorer/eat-mate-api/internal/infrastructure/db/models"
+)
+
+type GroupRepository struct {
+	db *db.Db
+}
+
+func NewGroupRepository(db *db.Db) *GroupRepository {
+
+	return &GroupRepository{db}
+}
+
+func (r GroupRepository) Create(entity entities.GroupEntity) (entities.GroupEntity, error) {
+
+	var group models.GroupModel
+
+	group.FromEntity(entity)
+
+	tx := r.db.Client.Begin()
+
+	err := tx.Create(&group).Error
+	if err != nil {
+		tx.Rollback()
+		return entities.GroupEntity{}, exceptions.NewRepositoryError(err)
+	}
+
+	entityMembers := entity.GetMembers()
+	members := make([]models.GroupMemberModel, len(entityMembers))
+
+	for i, e := range entityMembers {
+		e.SetGroupID(group.ID)
+		members[i].FromEntity(e)
+	}
+
+	err = tx.Create(&members).Error
+	if err != nil {
+		tx.Rollback()
+		return entities.GroupEntity{}, exceptions.NewRepositoryError(err)
+	}
+
+	entityPlaceBranchOptions := entity.GetPlaceBranchOptions()
+	placeBranchOptions := make([]models.PlaceBranchModel, len(entityPlaceBranchOptions))
+
+	for i, e := range entityPlaceBranchOptions {
+		placeBranchOptions[i].FromEntity(e)
+	}
+
+	err = tx.Model(&group).Association("PlaceBranchOptions").Append(&placeBranchOptions)
+	if err != nil {
+		tx.Rollback()
+		return entities.GroupEntity{}, exceptions.NewRepositoryError(err)
+	}
+
+	tx.Commit()
+
+	return group.ToEntity(), nil
+}
