d1d349a3f191ee49512805b4ec4b1a98e7a497c3
sunsetsavorer <sunset.savorer@gmail.com>
2025-08-20 22:21:30 +0300

    feat: add `leave group` usecase

---
+++ internal/usecases/group/leave_usecase.go
@@ -0,0 +1,55 @@
+package group
+
+import (
+	"fmt"
+
+	"github.com/sunsetsavorer/eat-mate-api/internal/dtos"
+	"github.com/sunsetsavorer/eat-mate-api/internal/exceptions"
+	"github.com/sunsetsavorer/eat-mate-api/internal/usecases"
+)
+
+type LeaveGroupUseCase struct {
+	logger          usecases.LoggerInterface
+	groupRepository GroupRepositoryInterface
+	userRepository  UserRepositoryInterface
+}
+
+func NewLeaveGroupUseCase(
+	logger usecases.LoggerInterface,
+	groupRepository GroupRepositoryInterface,
+	userRepository UserRepositoryInterface,
+) *LeaveGroupUseCase {
+
+	return &LeaveGroupUseCase{
+		logger:          logger,
+		groupRepository: groupRepository,
+		userRepository:  userRepository,
+	}
+}
+
+func (uc LeaveGroupUseCase) Exec(dto dtos.LeaveGroupDTO) error {
+
+	_, err := uc.groupRepository.GetByID(dto.GetGroupID())
+	if err != nil {
+		return err
+	}
+
+	member, err := uc.groupRepository.GetMemberByID(dto.GetUserID(), dto.GetGroupID())
+	if err != nil {
+		return exceptions.NewBadRequestError(fmt.Errorf("you can't leave group you're not in"))
+	}
+
+	isGroupOwner := member.GetRole() == OWNER_ROLE
+
+	if isGroupOwner {
+		err = uc.groupRepository.DeactivateByID(dto.GetGroupID())
+	} else {
+		err = uc.groupRepository.RemoveMember(member)
+	}
+
+	if err != nil {
+		return exceptions.NewBadRequestError(fmt.Errorf("something went wrong"))
+	}
+
+	return nil
+}
